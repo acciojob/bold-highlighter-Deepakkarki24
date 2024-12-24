0fea5bc714c0c0c145ab21b96b655e90f60aee21
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 04:36:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let strongWord = document.querySelectorAll("strong");
+
 function highlight() {
-    //Write your code here
-
-
+  //Write your code here
+  strongWord.forEach((word) => {
+    word.style.color = "green";
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  strongWord.forEach((word) => {
+    word.style.color = "black";
+  });
 }
